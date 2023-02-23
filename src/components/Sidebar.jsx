b60a8e86b3bcd81e2c9a23c509e39d6a73f80e3b
Breno Alves <breno_ja@hotmail.com>
2023-02-22 21:35:21 -0300

    add useNavigate to home and login components

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,16 +1,24 @@
 import "./Sidebar.scss";
 
+import { useNavigate } from "react-router-dom";
+
 import logo_curso from "../assets/images/logo_curso.png";
 import CustomButton from "./CustomButton";
 
 const Sidebar = () => {
+    const navigate = useNavigate();
+
+    const handleSignOut = () => {
+        navigate("/login");
+    };
+
     return (
         <div className="sidebar-container">
             <div className="logo">
                 <img src={logo_curso} alt="Curso React Pro" />
             </div>
             <div className="sign-out">
-                <CustomButton onClickButton={() => console.log("Saiu")}>Sair</CustomButton>
+                <CustomButton onClickButton={handleSignOut}>Sair</CustomButton>
             </div>
         </div>
     );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,22 @@
 import "./Login.scss";
 
+import { useNavigate } from "react-router-dom";
+
 import logo_curso2 from "../assets/images/logo_curso2.png";
 import CustomButton from "../components/CustomButton";
 
 const Login = () => {
+    const navigate = useNavigate();
+
+    const handleSingInClick = () => {
+        navigate("/");
+    };
+
     return (
         <div className="login-container">
             <img src={logo_curso2} alt="Logo do curso REACT PRO" />
             <div className="button-container">
-                <CustomButton>Entrar</CustomButton>
+                <CustomButton onClickButton={handleSingInClick}>Entrar</CustomButton>
             </div>
         </div>
     );
